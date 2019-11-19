218b986dcb7df6dbf87927e9eb89669249e20e37
Roman Volyk <Roman_Volyk@epam.com>
2019-11-19 14:58:11 +0200

    Abstract factory was added

---
+++ AbstractFactory/Client.cs
@@ -0,0 +1,28 @@
+﻿using AbstractFactory.Abstractions;
+
+namespace AbstractFactory
+{
+    public class Client
+    {
+        private AbstractBottle _bottle;
+        private AbstractWater _water;
+        private AbstractCap _cap;
+        private AbstractLabel _label;
+
+        public Client(Abstractions.AbstractFactory factory)
+        {
+            _bottle = factory.CreateBottle();
+            _water = factory.CreateWater();
+            _cap = factory.CreateCap();
+            _label = factory.CreateLabel();
+        }
+
+        public void RunProcess()
+        {
+            _bottle.Interact(_water);
+            _bottle.Interact(_cap);
+            _bottle.Interact(_label);
+        }
+        
+    }
+}
---
+++ AbstractFactory/CocaCola/CocaColaBottle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AbstractFactory.Abstractions;
+
+namespace AbstractFactory.CocaCola
+{
+    class CocaColaBottle : AbstractBottle
+    {
+        public override void Interact(AbstractWater water)
+        {
+            Console.WriteLine($"{this} interact with {water}");
+        }
+
+        public override void Interact(AbstractCap cap)
+        {
+            Console.WriteLine($"{this} interact with {cap}");
+        }
+
+        public override void Interact(AbstractLabel label)
+        {
+            Console.WriteLine($"{this} interact with {label}");
+        }
+    }
+}
