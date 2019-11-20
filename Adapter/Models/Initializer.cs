4ceaac521844e1ea7264ec24865d622d3bdb6028
Roman Volyk <Roman_Volyk@epam.com>
2019-11-20 17:50:32 +0200

    Adapter was added

---
+++ Adapter/Models/Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Adapter.Models
+{
+    public static class Initializer
+    {
+        public static IEnumerable<Manufacturer> GetData()
+        {
+            var data = new List<Manufacturer>
+            {
+                new Manufacturer { City = "Italy", Name = "Alfa Romeo", Year = 2016 },
+                new Manufacturer { City = "UK", Name = "Aston Martin", Year = 2018 },
+                new Manufacturer { City = "USA", Name = "Dodge", Year = 2017 },
+                new Manufacturer { City = "Japan", Name = "Subaru", Year = 2016 },
+                new Manufacturer { City = "Germany", Name = "BMW", Year = 2015 }
+            };
+            return data;
+        }
+    }
+}
---
+++ Adapter/Models/Manufacturer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Adapter.Models
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Year { get; set; }
+    }
+}
