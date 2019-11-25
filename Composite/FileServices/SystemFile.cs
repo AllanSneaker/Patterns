d4b7cf025508e17b48507c12afba22499043b279
Roman Volyk <Roman_Volyk@epam.com>
2019-11-25 17:49:31 +0200

    Composite was added

---
+++ Composite/FileServices/SystemFile.cs
@@ -0,0 +1,18 @@
+﻿using Composite.Abstractions;
+
+namespace Composite.FileServices
+{
+    public sealed  class SystemFile : File
+    {
+        public SystemFile(string name) : base(name) { }
+        public override void Add(File file) { }
+
+        public override string ToString()
+        {
+            string output = string.Format($"{new string('-', Nesting)} {Name} \n");
+            Nesting = 0;
+
+            return output;
+        }
+    }
+}
