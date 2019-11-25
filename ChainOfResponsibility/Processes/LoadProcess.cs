bfc01318e0834e83f6444757c1d1056cf79fed25
Roman Volyk <Roman_Volyk@epam.com>
2019-11-25 13:14:44 +0200

    ChainOfResponsibility was added

---
+++ ChainOfResponsibility/Processes/LoadProcess.cs
@@ -0,0 +1,18 @@
+﻿using ChainOfResponsibility.Abstract;
+using System;
+
+namespace ChainOfResponsibility.Processes
+{
+    class LoadProcess : AbstractProcess
+    {
+        public override void Process()
+        {
+            Console.WriteLine(new string('_', 10) + "Load Process" + new string('_', 10));
+
+            if (_next != null)
+            {
+                _next.Process();
+            }
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,20 @@
+﻿using ChainOfResponsibility.Processes;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DownloadProcess downloadProcess = new DownloadProcess();
+            ExtractProcess extractProcess = new ExtractProcess();
+            LoadProcess loadProcess = new LoadProcess();
+
+            downloadProcess.SetNextProcess(extractProcess);
+            extractProcess.SetNextProcess(loadProcess);
+
+            downloadProcess.Process();
+            
+        }
+    }
+}
