54806b4a57e4727169440968a5cd6be3a70993eb
Roman Volyk <Roman_Volyk@epam.com>
2019-11-27 12:24:41 +0200

    Memento was added

---
+++ Memento/Services/MementoCareTaker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Memento.Services
+{
+    public class MementoCareTaker<T> : Stack<Memento<T>> where T : class
+    {
+        private int _capacity;
+
+        public MementoCareTaker(int capacity = 5)
+        {
+            _capacity = capacity;
+        }
+
+        public void SaveState(T state)
+        {
+            if (Count > _capacity)
+                Clear();
+            if (Count < _capacity)
+                Push(new Memento<T>(state));
+        }
+
+        public T GetPreviousState()
+        {
+            if (Count > 0)
+                return Pop().GetState();
+            else
+                return default(T);
+        }
+    }
+}
